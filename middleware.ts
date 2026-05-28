02a9db08160b1c8ba3d22f27fc0a8390f742bd85
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2026-05-28 21:22:47 +0700

    add vercle file

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const hostname = req.headers.get("host");
+
+  // ❌ bỏ qua domain chính
+  if (hostname?.includes("dse-vn.com")) {
+    return NextResponse.next();
+  }
+
+  // ❌ bỏ qua vercel domain
+  if (hostname?.includes("vercel.app")) {
+    return NextResponse.redirect(new URL("https://dse-vn.com", req.url));
+  }
+
+  return NextResponse.next();
+}
