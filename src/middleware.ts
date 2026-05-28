bc08a282314addebb105dcef1adc0e35e5f95d21
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2026-05-28 21:36:51 +0700

    add vercle file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,37 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import createMiddleware from "next-intl/middleware";
+
+// 1. i18n middleware
+const intlMiddleware = createMiddleware({
+  locales: ["vi", "en"],
+  defaultLocale: "vi",
+  localeDetection: true,
+});
 
 export function middleware(req: NextRequest) {
   const fullUrl = req.url;
 
-  console.log("fullur;", fullUrl);
+  console.log(fullUrl);
 
-  // ❌ bỏ qua domain chính
-  if (fullUrl.includes("dse-vn.com")) {
-    return NextResponse.next();
-  }
-
-  // ❌ vercel domain -> redirect
+  // ❌ redirect vercel -> domain chính
   if (fullUrl.includes("vercel.app")) {
-    return NextResponse.redirect("https://dse-vn.com/vi");
+    return NextResponse.redirect("https://dse-vn.com");
   }
 
-  return NextResponse.next();
+  // ❌ nếu domain chính thì bỏ qua logic redirect khác
+  if (fullUrl.includes("dse-vn.com")) {
+    return intlMiddleware(req);
+  }
+
+  // fallback
+  return intlMiddleware(req);
 }
+
+// 2. matcher gộp lại
+export const config = {
+  matcher: [
+    // bỏ qua API + static files + next internals
+    "/((?!api|_next|.*\\..*).*)",
+  ],
+};
